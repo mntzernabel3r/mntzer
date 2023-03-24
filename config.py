edc4f1608a675ab7c7c092e3c91bf6c610090e29
mntzernabel3r <128753331+mntzernabel3r@users.noreply.github.com>
2023-03-24 03:28:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,32 +5,32 @@ admins = {}
 load_dotenv()
 
 # client vars
-API_ID = int(getenv("API_ID", "7452578")) #لا تغير هاذة القيمة
-API_HASH = getenv("API_HASH", "061d67ee8eed9368c5cadabb4aa21efc")#لا تغير هاذة القيمة
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-SESSION_NAME = getenv("SESSION_NAME", "")
+API_ID = int(getenv("API_ID", "23398820")) #لا تغير هاذة القيمة
+API_HASH = getenv("API_HASH", "95ad635217d1b6b193c9c9be0254cc2d")#لا تغير هاذة القيمة
+BOT_TOKEN = getenv("BOT_TOKEN", "5945051414:AAF5Vm4GRSkCfXE_Alq_IE4XaBwWh9AWNb8")
+SESSION_NAME = getenv("SESSION_NAME", "AgCY9EXyCNzoYTjLx61YEGl8Syzfz4m3Nwp3k7HWSrH0OFrN6Xz1fpgkgkZu8YoqyptmZ2T7U7Lt8iVl5adEVn5rFoNeJJevOetChP-UeE8TO2BhOnzn6xaH3maduay7behxPh7z0BHavpQflbP_VP-bsKlVKwqZsxp7QpNDfWcnd1x5SCXeXGMXbFo8SOI_Ex2bT6SlCKra-bKaGdsn0wM7yHIu3VcDoQ5n5EU96dUO1iLLEJr6uH1O4dvS3dGb0Rd_qdmPfAB3IF7I7qnSRUB0NjFP7fbhqOzu8_bi-VE1twvVs2oDuLSVLTxzhRgPI464koBmdQotJtxRqAqeZ-cXAAAAAVaEJhIA")
 
 # mandatory vars
-OWNER_USERNAME = getenv("OWNER_USERNAME", "rr8r9") # @ هنا ضع يوزر حسابك بدون 
-ALIVE_NAME = getenv("ALIVE_NAME", "sonng") # هنا ضع اسم حسابك
-BOT_USERNAME = getenv("BOT_USERNAME", "") # @ هنا ضع يوزر البوت بدون 
+OWNER_USERNAME = getenv("OWNER_USERNAME", "AT_MGF") # @ هنا ضع يوزر حسابك بدون 
+ALIVE_NAME = getenv("ALIVE_NAME", "music") # هنا ضع اسم حسابك
+BOT_USERNAME = getenv("BOT_USERNAME", "MUC_KBOT") # @ هنا ضع يوزر البوت بدون 
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/X02lx/RrRRR") 
 UPSTREAM_BRANCH = getenv("UPSTREM_BRANCH", "main") #لا تغير هاذة القيمة
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60")) #لا تغير هاذة القيمة
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "rr8r9") # @ هنا ضغ يوزر كروبك بدون 
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "xl444") # @ هنا ضغ يوزر قناتك بدون
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "anime6o") # @ هنا ضغ يوزر كروبك بدو
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "anime6lf") # @ هنا ضغ يوزر قناتك بدون
 
 # database, decorators, handlers mandatory vars
 MONGODB_URL = getenv("MONGODB_URL", "mongodb+srv://veez:mega@cluster0.heqnd.mongodb.net/veez?retryWrites=true&w=majority")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! . $").split())
-OWNER_ID = list(map(int, getenv("OWNER_ID", "1854384004").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "5746468370").split()))
                                              #هنا ضع ايدي المطور فوق و الاعلئ
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1854384004").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5746468370").split()))
 
 # image resources vars
-IMG_1 = getenv("IMG_1", "https://te.legra.ph/file/2a726c634dbc3b9e8f451.png")
-IMG_2 = getenv("IMG_2", "https://te.legra.ph/file/90e3b3aeb77e3e598d66d.png")
-IMG_3 = getenv("IMG_3", "https://te.legra.ph/file/d70bb6fa92728763c671f.png")
-IMG_4 = getenv("IMG_4", "https://te.legra.ph/file/430dcf25456f2bb38109f.png")
-IMG_5 = getenv("IMG_5", "https://te.legra.ph/file/cd5c96a3c7e8ae1913ef3.png")
-ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph/file/c83b000f004f01897fe18.png")
+IMG_1 = getenv("IMG_1", "https://telegra.ph/file/f7d0b8bf1ede6e2c2dc9e.jpg")
+IMG_2 = getenv("IMG_2", "https://telegra.ph/file/3bebe90db0c9399d82881.jpg")
+IMG_3 = getenv("IMG_3", "https://telegra.ph/file/5199a41d34f118f942699.jpg")
+IMG_4 = getenv("IMG_4", "https://telegra.ph/file/584a7dbfa8d6d6b24b236.jpg")
+IMG_5 = getenv("IMG_5", "https://telegra.ph/file/2412cf577e0f61f843e6f.jpg")
+ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph/file/51b3539289e6648c3b062.jpg")
